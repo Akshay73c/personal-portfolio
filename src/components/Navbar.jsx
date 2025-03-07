93cb3af699e282b3213f14288188a58a855c6fa5
Akshay73c <akshay.chavan20@vit.edu>
2025-03-07 16:51:59 +0530

    fix Navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 const Navbar = () => {
   return (
-    <div className="flex justify-between h-24 w-full fixed top-0 left-0 p-6 pt-8 px-[3.125rem] bg-[#bfd6c4]/95 backdrop-blur-[1px] z-[1000] shadow-md">
+    <div className="flex justify-between h-24 w-screen fixed top-0 left-0 p-6 pt-8 bg-[#bfd6c4]/95 backdrop-blur-[1px] z-[1000] shadow-md">
       <div className="text-xl font-semibold mr-6">Akshay Chavan</div>
       <div>
-        <div className="flex justify-center space-x-10 text-md font-normal ">
+        <div className="flex justify-center space-x-1 md:space-x-10 text-sm md:text-xl font-normal ">
           <a href="#about-section">About</a>
           <a href="#projects-section">Projects</a>
           <a href="#skills-section">Skills</a>
